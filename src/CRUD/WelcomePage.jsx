c9e0175167e305f95050542f3b8d1e6b7381a7e2
kannan.j <113454965+kannan304@users.noreply.github.com>
2024-06-21 21:34:32 +0530

    Update WelcomePage.jsx

--- src/CRUD/WelcomePage.jsx
+++ src/CRUD/WelcomePage.jsx
@@ -7,7 +7,7 @@ const WelcomePage = () => {
     <div  className={style.welcome} >
       <div id={style.content}>
         <p className={style.line1}>The Only Platform To</p>
-        <p className={style.line2}>Use free DataBase for Pratice</p>
+        <p className={style.line2}>Use free DataBase for Practice</p>
         <p className={style.line3}>Welcome to our Employee Management System! With our system, you can easily perform CRUD (Create, Read, Update, Delete) operations on a database of employees.<br/></p>
         <button className={style.Button1}>CONTACT US</button>
         <button className={style.Button2}><i class="fa-regular fa-message"></i> FeedBack</button>
@@ -29,3 +29,4 @@ const WelcomePage = () => {
 }
 
 export default WelcomePage
+ 
